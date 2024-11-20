f359916816a03acf0c1a14c7f7a63fff1979958d
kmj0973 <kmj0973@naver.com>
2024-11-20 20:02:38 +0900

    Title: 최소공배수 구하기, Time: 5082ms, Memory: 17MB, Status: Time Limit Exceed - CodeTree

--- 241120/최소공배수 구하기/find-the-least-common-multiple.js
+++ 241120/최소공배수 구하기/find-the-least-common-multiple.js
@@ -3,16 +3,18 @@ const fs = require("fs")
 const [n,m] = fs.readFileSync(0).toString().trim().split(" ");
 
 var answer =0;
+var minMul = 2;
 var mul = 2;
 var minNum = Math.min(n,m);
 var maxNum = Math.max(m,n);
-for(var i = 2;i<=(n*m);i++){
+while(1){
     if(minNum==maxNum){
         answer=minNum;
         console.log(answer)
         return;
     }
-    minNum = n*i;
+    minNum = n*minMul;
+    minMul++;
     if(minNum>maxNum){
         maxNum = m*mul;
         mul++;
