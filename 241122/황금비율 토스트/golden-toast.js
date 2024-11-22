c3d26091eb7c2aef2f9ce8ba1c9d38cf58c4f010
kmj0973 <kmj0973@naver.com>
2024-11-22 14:50:41 +0900

    Title: 황금비율 토스트, Time: 199ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241122/황금비율 토스트/golden-toast.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const dataArr = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n,m] = dataArr[0].split(" ").map(Number);
+
+const str = dataArr[1].split("");
+var iterator = n-1;
+for(var i = 2; i<m+2; i++){
+    const arr = dataArr[i].split(" ")
+
+    if(arr[0]=="L"){
+        if(iterator!=0) iterator--;
+    }else if(arr[0]=="R"){
+        if(iterator!=str.length) iterator++;
+    }else if(arr[0]=="D"){
+        if(iterator!=str.length) str.splice(iterator+1,1);
+    }else if(arr[0]=="P"){
+        str.splice(iterator+1,0,arr[1]);
+        iterator++;
+    }
+}
+
+console.log(str.join(""))
