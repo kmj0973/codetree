7360e58beba8d0f4d905d16a7fb27bc4ad5aaaa8
kmj0973 <kmj0973@naver.com>
2024-11-27 11:14:08 +0900

    Title: 거품 정렬 구현, Time: 177ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241127/거품 정렬 구현/implement-bubble-sort.js
+++ 241127/거품 정렬 구현/implement-bubble-sort.js
@@ -6,7 +6,7 @@ var arr = dataArray[1].split(" ");
 let isTrue = true;
 do{
     isTrue = true
-    for(var i = 0; i<dataArray[0]; i++){
+    for(var i = 0; i<dataArray[0]-1; i++){
         if(arr[i]>arr[i+1]){
             var tm = arr[i];
             arr[i] = arr[i+1];
