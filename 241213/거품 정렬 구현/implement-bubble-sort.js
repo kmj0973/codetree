e90c682f44d8e51454fc614b2381c49633a9e74d
kmj0973 <kmj0973@naver.com>
2024-12-13 13:47:12 +0900

    Title: 거품 정렬 구현, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241213/거품 정렬 구현/implement-bubble-sort.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const dataArray = fs.readFileSync(0).toString().trim().split("\n");
+
+var arr = dataArray[1].split(" ").map(Number);
+let isTrue = true;
+do{
+    isTrue = true
+    for(var i = 0; i<dataArray[0]-1; i++){
+        if(arr[i]>arr[i+1]){
+            var tm = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = tm;
+            isTrue = false;
+        }  
+    }
+
+}while(isTrue!=true)
+
+console.log(arr.join(" "));
