52c8e27cc336cfceae4cbe3079c2a8ecc0837778
kmj0973 <kmj0973@naver.com>
2025-01-02 17:05:02 +0900

    Title: 정수 명령 처리, Time: 201ms, Memory: 47MB, Status: Runtime Error - CodeTree

---
+++ 250102/정수 명령 처리/process-numeric-commands.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0).toString().split("\n");
+
+const n = Number(input[0]);
+const arr = [];
+for(var i = 1; i < n+1; i++){
+    var a = input[i].split(" ");
+
+    if(a[0]=="push"){
+        arr.push(a[1]);
+    }else if(a[0]=="size"){
+        console.log(arr.length);
+    }else if(a[0]=="empty"){
+        if(arr.length == 0) console.log("1");
+        else console.log("0");
+    }else if(a[0]=="pop"){
+        console.log(arr.pop());
+    }else{
+        console.log(arr.top());
+    }
+}
